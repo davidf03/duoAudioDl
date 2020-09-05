a9d35acf3bd88f2a61adae2b549877c39e487d02
david <dfriesen1183@yahoo.ca>
2020-09-05 10:58:41 -0400

    downloading files working

--- src/background.ts
+++ src/background.ts
@@ -1,1 +1,38 @@
-console.log('hello world background')
+const pattern = 'https://*.cloudfront.net/astrid/*';
+const files = [];
+
+browser.webRequest.onBeforeRequest.addListener(
+  downloadFile,
+  {urls:[pattern]},
+);
+
+function downloadFile(requestDetails) {
+  const url = requestDetails.url;
+
+  let folder = requestDetails.originUrl.split('/')
+  folder = folder[folder.length - 2];
+
+  const name = url.split('astrid/')[1];
+  let folderIndex = files.findIndex(f => f.folder === folder);
+
+  if (folderIndex !== -1) {
+    if (files[folderIndex].files.includes(name)) {
+      return;
+    }
+    folderIndex = files.length;
+    files.push({
+      folder,
+      files: []
+    });
+  }
+  files[folderIndex].files.push(name);
+
+  let downloading = browser.downloads.download({
+    url: url,
+    filename: `duoAudioDlFiles/${folder}/${name}.mp3`,
+  }).catch(() => {
+    files.splice(files.indexOf(name), 1);
+  });
+
+  console.log(files);
+}
