efcdbd292c06aa4c2eb22a79508da74703c13eff
david <dfriesen1183@yahoo.ca>
2020-09-06 12:04:27 -0400

    pending cards icon status

--- src/background.ts
+++ src/background.ts
@@ -1,13 +1,20 @@
 const pattern = 'https://*.cloudfront.net/astrid/*';
 let duoAudioDl;
 
+browser.storage.local.get('duoAduioDl').then(res => {
+  if (res?.duoAudioDl?.cardQueue?.length) {
+    return;
+  }
+  setIconToPending();
+});
+
 browser.webRequest.onBeforeRequest.addListener(
   addFileToQueue,
   { urls: [pattern] },
 );
 
 async function addFileToQueue(req) {
-  duoAudioDl || await browser.storage.local.get('duoAudioDl').then(res => duoAudioDl = res.duoAudioDl ?? {});
+  duoAudioDl || await browser.storage.local.get('duoAudioDl').then(res => ({ duoAudioDl = {} } = res));
 
   duoAudioDl.cardQueue ??= [];
 
@@ -18,19 +25,21 @@ async function addFileToQueue(req) {
   // skill = skill[skill.length - 2];
 
   let skillIndex = duoAudioDl.cardQueue.findIndex(f => f.skill === skill);
-  if (skillIndex !== -1) {
-    if (duoAudioDl.cardQueue[skillIndex].cards.includes(c => c.url === url)) {
-      return;
-    }
-  } else {
+  if (skillIndex === -1) {
     skillIndex = duoAudioDl.cardQueue.length;
     duoAudioDl.cardQueue.push({
       skill,
       cards: []
     });
+  } else if (duoAudioDl.cardQueue[skillIndex].cards.includes(c => c.url === url)) {
+    return;
   }
   duoAudioDl.cardQueue[skillIndex].cards.push({ url, pending: true });
-  console.log(duoAudioDl);
 
   await browser.storage.local.set({ duoAudioDl });
+  setIconToPending();
+}
+
+async function setIconToPending() {
+  browser.browserAction.setIcon({ path: './icons/48-pending.png' });
 }
