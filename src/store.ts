9012eb8f2a5cb19073bdc9c27132d1ede9918735
david <dfriesen1183@yahoo.ca>
2020-09-17 21:42:06 -0400

    better store generics

--- src/store.ts
+++ src/store.ts
@@ -1,7 +1,7 @@
 import { writable } from 'svelte/store';
 import ankiconnect from './contentScripts/ankiConnect';
 import ankiParser from './util/ankiParser';
-import type { iCardList } from './interfaces/iCards';
+import type { iCardList, iCard } from './interfaces/iCards';
 import type { iPrefs } from './interfaces/iPrefs';
 import type { iNameAndId } from './interfaces/iNameAndId';
 import type { iTemplate } from './interfaces/iTemplate';
@@ -46,124 +46,108 @@ export const playingAudioId = writable();
 export const expandedCardId = writable();
 
 // stores to represent the state of getting stores (lol)
-export const loadingStore = writable(true as boolean);
-export const loadingAnkiCards = writable(true as boolean);
-export const loadingAnkiDeckNamesAndIds = writable(true as boolean);
-export const loadingAnkiTemplateNamesAndIds = writable(true as boolean);
-export const loadingAnkiTemplates = writable(true as boolean);
+export const loadingStore = writable(true);
+export const loadingAnkiCards = writable(true);
+export const loadingAnkiDeckNamesAndIds = writable(true);
+export const loadingAnkiTemplateNamesAndIds = writable(true);
+export const loadingAnkiTemplates = writable(true);
 
-// getting local
+// init permanent stores
 browser.storage.local.get([
   'lngs',
   'lng',
   'queue',
   'ignored',
-  'prefs',
-
-  'history',
-  'deckNamesAndIds',
-  'templateNamesAndIds',
-  'templates'
+  'prefs'
 ]).then(res => {
   const {
     lngs: lngsLocal,
     lng: lngLocal,
     queue: queueLocal,
     ignored: ignoredLocal,
-    prefs: prefsLocal,
-
-    history: historyLocal,
-    deckNamesAndIds: deckNamesAndIdsLocal,
-    templateNamesAndIds: templateNamesAndIdsLocal,
-    templates: templatesLocal
+    prefs: prefsLocal
   } = res;
 
-  lngs.set(lngsLocal || {} as iCardList);
-  lng.set(lngLocal || lngsLocal && lngsLocal[0] || '' as string);
-  queue.set(queueLocal || {} as iCardList);
-  ignored.set(ignoredLocal || {} as iCardList);
-  prefs.set(prefsLocal || {} as iPrefs);
+  lngsLocal && lngs.set(lngsLocal);
+  lngLocal && lng.set(lngLocal) || lngsLocal?.[0] && lng.set(lngsLocal[0]);
+  queueLocal && queue.set(queueLocal);
+  ignoredLocal && ignored.set(ignoredLocal);
+  prefsLocal && prefs.set(prefsLocal);
 
-  history.set(historyLocal || {} as iCardList);
-  deckNamesAndIds.set(deckNamesAndIdsLocal || [] as iNameAndId[]);
-  templateNamesAndIds.set(templateNamesAndIdsLocal || {} as iNameAndId[]);
-  templates.set(templatesLocal || {} as iCardList);
-
-  loadingStore.set(false as boolean);
+  loadingStore.set(false);
 });
 
-// getting updates
-// getAnkiUpdates('findCards', 6, updateLocalHistory);
-getAnkiUpdates(
+
+initJointStore(
+  'deckNamesAndIds',
   'deckNamesAndIds', 6,
-  ankiParser.namesAndIds.from,
-  deckNamesAndIds, loadingAnkiDeckNamesAndIds
+  deckNamesAndIds, loadingAnkiDeckNamesAndIds,
+  ankiParser.namesAndIds.from
 );
-getAnkiUpdates(
+initJointStore(
+  'templateNamesAndIds',
   'modelNamesAndIds', 6,
-  ankiParser.templates.from,
-  templateNamesAndIds, loadingAnkiTemplateNamesAndIds
+  templateNamesAndIds, loadingAnkiTemplateNamesAndIds,
+  ankiParser.templates.from
 );
-// getAnkiUpdates('findModels', 6, updateLocalTemplates);
+// initJointStore(
+//   'history',
+//   'findCards', 6,
+//   history, loadingAnkiCards,
+//   integrateHistoryUpdates
+// );
+// getAnkiUpdates(
+//   'templates',
+//   [],
+//   'findModels', 6,
+//   templates, loadingAnkiTemplates,
+//   integrateTemplateUpdates
+// );
+
 
 interface iStore<T, U> {
   useLocalStorage: ()=>U;
-  set: (val:T)=>U;
+  set: (val:T)=>U
 }
-interface iParserMethod<T, U> {
-  (data:T): U;
+interface iUpdatesCallback<T, U> {
+  (data:T, localData?:U): U;
 }
-interface iUpdatesCallback<T, U, V> {
-  (data:T, parserMethod:iParserMethod<T, U>, store:iStore<U, V>, storeLoading:iStore<boolean, V>): void;
-}
-async function getAnkiUpdates
+
+async function initJointStore
   <T, U, V>(
+    localKey:string,
     action:string,
     version:number,
-    parserMethod:iParserMethod<T, U>,
-    store:iStore<U,V>,
-    storeLoading:iStore<boolean,V>,
-    callback:iUpdatesCallback<T, U, V> = getAnkiUpdatesCallback
+    store:iStore<U, V>,
+    storeLoader:iStore<boolean, V>,
+    callback:iUpdatesCallback<T, U>
   ): Promise<any> {
-  ankiconnect.invoke(action, version).then(res => {
-    if (loadingStore) {
-      loadingStore.subscribe(val => !val && callback(res as T, parserMethod, store, storeLoading))
-      return;
+  // load anki and local simultaneously; last to load calls callback; if local loads first set store while updates from anki load
+  const localPromise = browser.storage.local.get(localKey);
+  const ankiPromise = ankiconnect.invoke(action, version);
+  Promise.allSettled([localPromise, ankiPromise]).then(res => {
+    let data:U;
+    if (res[1].status === 'rejected') {
+      data = res[0].value;
+    } else {
+      data = callback(res[1].value as T, res[0].value as U); // TODO is potentially overloading ankiParser illegal in ts?
     }
-    return callback(res as T, parserMethod, store, storeLoading);
+    store.useLocalStorage();
+    data && store.set(data);
+    storeLoader.set(false);
   });
 }
-function getAnkiUpdatesCallback
-  <T, U, V>(
-    data:T,
-    parserMethod:iParserMethod<T, U>,
-    store:iStore<U, V>,
-    storeLoading:iStore<boolean, V>
-  ): void {
-  store.useLocalStorage();
-  store.set(parserMethod(data));
-  storeLoading.set(false);
+
+function integrateHistoryUpdates(data:iCardAnki[], localData:iCardList): iCardList {
+  const updatedData:iCardList = localData;
+  // const ankiCards:iCard[] = ankiParser.cards.from(data);
+  // cards.forEach(); // by some id, update information in matching cards in history
+  return updatedData;
 }
-// function updateLocalHistory (data:iCardAnki[]): void {
-//   // const ankiCards = ankiParser.cards.from(data);
-//   // const historyLocal = history;
-//   // cards.forEach(); // by some id, update information in matching cards in history
-//   history.useLocalStorage();
-//   // history.set(historyLocal);
-//   loadingAnkiCards.set(false);
-// }
-function updateLocalDeckNamesAndIds (data:iNamesAndIdsAnki): void {
-  deckNamesAndIds.useLocalStorage();
-  deckNamesAndIds.set(ankiParser.namesAndIds.from(data));
-  loadingAnkiDeckNamesAndIds.set(false);
+
+function integrateTemplateUpdates(data:iTemplateAnki[], localData:iTemplate[]): iTemplate[] {
+  const updatedData:iTemplate[] = localData;
+  // const ankiTemplates:iTemplate[] = ankiParser.templates.from(data);
+  // templates.forEach(); // delete previous templates, but keep ones for cards in queue that use them/lang prefs
+  return updatedData;
 }
-// function updateLocalTemplateNamesAndIds (data:iNamesAndIdsAnki): void {
-//   templateNamesAndIds.useLocalStorage();
-//   templateNamesAndIds.set(ankiParser.namesAndIds.from(data));
-//   loadingAnkiTemplateNamesAndIds.set(false);
-// }
-// function updateLocalTemplates (data:iTemplateAnki[]): void {
-//   templates.useLocalStorage();
-//   templates.set(ankiParser.templates.from(data));
-//   loadingAnkiTemplates.set(false);
-// }
